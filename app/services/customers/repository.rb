276dfc01cea1a0f69397a20111764a23b54f049c
Jose Lloret <jose.lloret@flywire.com>
2020-02-11 01:30:31 +0100

    Customer::Repository WIP

--- app/services/customers/repository.rb
+++ app/services/customers/repository.rb
@@ -4,12 +4,25 @@ module Carpanta
       class Repository
         class << self
           def create(customer)
+            attributes = customer.serializable_hash
+
+            result = Storage.create(attributes)
+            if result.errors.empty?
+              customer.id = result.id
+              customer.created_at = result.created_at
+              customer.updated_at = result.updated_at
+            else
+              customer.errors.merge!(result.errors)
+            end
+
+            customer
           end
         end
-      end
 
-      class Storage < ActiveRecord::Base
-        self.table_name = 'customers'
+        class Storage < ActiveRecord::Base
+          self.table_name = 'customers'
+          validates_uniqueness_of :email
+        end
       end
     end
   end
