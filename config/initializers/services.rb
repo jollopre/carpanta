af4da1ea4af509c02f8634f8b91193ebdb502fde
Jose Lloret <jollopre@gmail.com>
2020-02-16 14:39:49 +0100

    Services::Task using injected repository (#13)

--- config/initializers/services.rb
+++ config/initializers/services.rb
@@ -1,6 +1,12 @@
 require 'app/services/customers'
+require 'app/services/tasks'
 require 'app/repositories/customer'
+require 'app/repositories/task'
 
 Carpanta::Services::Customers.configure do |config|
   config.repository = Carpanta::Repositories::Customer
+end
+
+Carpanta::Services::Tasks.configure do |config|
+  config.repository = Carpanta::Repositories::Task
 end
