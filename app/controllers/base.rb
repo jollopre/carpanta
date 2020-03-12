ee951e8d4eeab379b42ee10697500a57c4b57858
Jose Lloret <jose.lloret@flywire.com>
2020-03-12 01:30:36 +0100

    Setting requests log for sinatra

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -3,8 +3,16 @@ require 'sinatra/base'
 module Carpanta
   module Controllers
     class Base < Sinatra::Base
-      set :root, File.join(ENV.fetch('ROOT_PATH'), 'app')
-      set :haml, format: :html5
+      configure do
+        set :root, File.join(ENV.fetch('ROOT_PATH'), 'app')
+        set :haml, format: :html5
+        set :environment, ENV.fetch('RACK_ENV')
+        set :logging, true
+
+        file = File.new(File.join(ENV.fetch('ROOT_PATH'), 'log', ENV.fetch('RACK_ENV') + '.log'), 'a+')
+        file.sync = true
+        use Rack::CommonLogger, file
+      end
     end
   end
 end
