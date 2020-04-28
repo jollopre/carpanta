5c4b14797b623b0098df690203d9769709a77342
Jose Lloret <jose.lloret@flywire.com>
2020-04-28 08:54:02 +0200

    Domain::Offers::Offer completed

---
+++ domain/offers/offer.rb
@@ -0,0 +1,36 @@
+require 'domain/shared/entity'
+
+module Carpanta
+  module Domain
+    module Offers
+      class Offer < Shared::Entity
+        ZERO = 0.freeze
+        attr_reader :tasks, :price
+
+        validates_presence_of :tasks, :price
+        validates_numericality_of :price, only_integer: true, greater_than: ZERO
+
+        def name
+          tasks.to_sentence
+        end
+
+        private
+
+        def initialize(tasks: nil, price: nil)
+          @tasks = tasks
+          @price = price
+        end
+
+        class << self
+          def build(tasks: nil, price: nil)
+            offer = new(tasks: tasks, price: price)
+
+            offer.validate
+
+            offer
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/domain/factories/offer.rb
@@ -0,0 +1,12 @@
+require 'domain/offers/offer'
+
+FactoryBot.define do
+  factory :offer, class: Carpanta::Domain::Offers::Offer do
+    tasks { ['Cutting with scissor', 'Shampooing'] }
+    price { 2200 }
+  end
+
+  initialize_with do
+    Carpanta::Domain::Offers::Offer.build(attributes)
+  end
+end
