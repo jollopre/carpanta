2b14c25b711b945b8bb652e48002d6845c035cac
Jose Lloret <jose.lloret@flywire.com>
2020-04-29 09:31:06 +0200

    Fixing broken tests

--- spec/domain/factories/offer.rb
+++ spec/domain/factories/offer.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
   factory :offer, class: Carpanta::Domain::Offers::Offer do
     tasks { ['Cutting with scissor', 'Shampooing'] }
     price { 2200 }
-  end
 
-  initialize_with do
-    Carpanta::Domain::Offers::Offer.build(attributes)
+    initialize_with do
+      Carpanta::Domain::Offers::Offer.build(attributes)
+    end
   end
 end
