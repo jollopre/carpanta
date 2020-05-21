4ee694eda74f004a482b7ca319a0addc1919e2d2
Jose Lloret <jose.lloret@flywire.com>
2020-05-21 08:32:20 +0200

    Fixing seed.rb

--- db/seed.rb
+++ db/seed.rb
@@ -1,43 +1,43 @@
+require 'domain/offers/offer'
+require 'domain/offers/repository'
 require 'domain/customers/service'
-require 'app/services/sessions'
-require 'app/queries/find_tasks'
+require 'app/commands/create_appointment'
 
 lambda do
-  tasks = lambda do
+  offers_attrs =
     [
-      { name: 'Corte Caballero con lavado', price: 1000 },
-      { name: 'Arreglo de barba', price: 500 },
-      { name: 'Arreglo flequillo', price: 300 },
-      { name: 'Corte nin@', price: 500 },
-      { name: 'Lavar y secar', price: 1200 },
-      { name: 'Retoque "mentira", lavar y secar', price: 1800 },
-      { name: 'Corte y secado "al aire"', price: 1500 },
-      { name: 'Corte y secado', price: 2500 },
-      { name: 'Color y secado', price: 3500 },
-      { name: 'Color, secado y corte', price: 4000 },
-      { name: 'Mechas para iluminar en frontal', price: 1500 },
-      { name: 'Mechas pelo corto "al aire"', price: 2500 },
-      { name: 'Mechas pelo corto y secado', price: 3000 },
-      { name: 'Mechas pelo corto, corte y secado', price: 3500 },
-      { name: 'Mechas y secado', price: 4000 },
-      { name: 'Mechas, secado y corte', price: 5000 },
-      { name: 'Degradado y secado', price: 4500 },
-      { name: 'Degradado con retoque raiz y secado', price: 6000 },
-      { name: 'Degradado con retoque de raiz, corte y secado', price: 6500 },
-      { name: 'Matiz', price: 1500 },
-      { name: 'Deco arrastre', description: 'hasta 3 decoloraciones', price: 9000 },
-      { name: 'Color global', description: 'hasta 2 tubos', price: 5000 },
-      { name: 'Alisado ORGANIC', description: 'Por gramo', price: 150 },
-      { name: 'Permanente', price: 3500 },
-      { name: 'Peinado de fallera', price: 3000 },
-      { name: 'Peinado de fallera cosido', price: 2500 },
-      { name: 'Recogido fiesta', price: 3000 },
-      { name: 'Tratamiento ELUMEN', description: '0.60 por gramo', price: 3000 },
-      { name: 'Tratamiento REPARADOR y secado', price: 3500 }
-    ]
-  end
+      { tasks: ['Corte Caballero con lavado'], price: 1000 },
+      { tasks: ['Arreglo de barba'], price: 500 },
+      { tasks: ['Arreglo flequillo'], price: 300 },
+      { tasks: ['Corte nin@'], price: 500 },
+      { tasks: ['Lavar', 'secar'], price: 1200 },
+      { tasks: ['Retoque "mentira"', 'lavar', 'secar'], price: 1800 },
+      { tasks: ['Corte', 'secado "al aire"'], price: 1500 },
+      { tasks: ['Corte', 'secado'], price: 2500 },
+      { tasks: ['Color', 'secado'], price: 3500 },
+      { tasks: ['Color', 'secado', 'corte'], price: 4000 },
+      { tasks: ['Mechas para iluminar en frontal'], price: 1500 },
+      { tasks: ['Mechas pelo corto "al aire"'], price: 2500 },
+      { tasks: ['Mechas pelo corto', 'secado'], price: 3000 },
+      { tasks: ['Mechas pelo corto', 'corte', 'secado'], price: 3500 },
+      { tasks: ['Mechas', 'secado'], price: 4000 },
+      { tasks: ['Mechas', 'secado', 'corte'], price: 5000 },
+      { tasks: ['Degradado', 'secado'], price: 4500 },
+      { tasks: ['Degradado con retoque raiz', 'secado'], price: 6000 },
+      { tasks: ['Degradado con retoque de raiz', 'corte', 'secado'], price: 6500 },
+      { tasks: ['Matiz'], price: 1500 },
+      { tasks: ['Deco arrastre'], price: 9000 },
+      { tasks: ['Color global'], price: 5000 },
+      { tasks: ['Alisado ORGANIC'], price: 150 },
+      { tasks: ['Permanente'], price: 3500 },
+      { tasks: ['Peinado de fallera'], price: 3000 },
+      { tasks: ['Peinado de fallera cosido'], price: 2500 },
+      { tasks: ['Recogido fiesta'], price: 3000 },
+      { tasks: ['Tratamiento ELUMEN'], price: 3000 },
+      { tasks: ['Tratamiento REPARADOR', 'secado'], price: 3500 }
+  ]
 
-  customers_attrs = lambda do
+  customers_attrs =
     [
       { name: 'Ben', surname: 'Dover', email: 'ben.dover@carpanta.com', phone: '600111222' },
       { name: 'Hermione', surname: 'Byrne', email: 'hermione.byrne@carpanta.com', phone: '600111222' },
@@ -49,20 +49,30 @@ lambda do
       { name: 'Fatima', surname: 'Sanchez', email: 'fatima.sanchez@carpanta.com', phone: '600111222' },
       { name: 'Pearl', surname: 'Baxter', email: 'pearl.baxter@carpanta.com', phone: '600111222' },
       { name: 'Molly', surname: 'Barron', email: 'molly.barron@carpanta.com', phone: '600111222' }
-    ]
+  ]
+
+  offers = offers_attrs.map do |offer_attrs|
+    offer = Carpanta::Domain::Offers::Offer.build(offer_attrs)
+    raise "Error building offer. Details: #{offer.errors.to_h}" unless offer.errors.empty?
+
+    Carpanta::Domain::Offers::Repository.save!(offer)
+    offer
   end
 
-  create_session = lambda do |customer, task|
-    Carpanta::Services::Sessions.create!(customer_id: customer.id, task_id: task.id, price: task.price)
+  customers = customers_attrs.map do |customer_attrs|
+    Carpanta::Domain::Customers::Service.save!(customer_attrs)
   end
 
-  tasks = tasks.call.map { |task| Carpanta::Services::Tasks.create!(task) }
-  customers = customers_attrs.call.map { |customer_attrs| Carpanta::Domain::Customers::Service.save!(customer_attrs) }
-
-  customers.each do |customer|
-    (1..10).each do
-      create_session.call(customer, tasks.sample)
+  create_appointment = lambda do |customer, offer|
+    result = Carpanta::Commands::CreateAppointment.call(customer_id: customer.id, offer_id: offer.id, starting_at: Time.now, duration: rand(30..60))
+    result.failure do |errors|
+      raise "Error creating appointment. Details: #{errors}"
     end
   end
 
+  customers.each do |customer|
+    (1..10).each do
+      create_appointment.call(customer, offers.sample)
+    end
+  end
 end.call
