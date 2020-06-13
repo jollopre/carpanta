4e9d1703437c59145ed14bbc1a510a416860a1aa
Jose Lloret <jose.lloret@flywire.com>
2020-06-13 20:07:10 +0200

    [#48] Using Logger pointing at STDOUT

--- config/app.rb
+++ config/app.rb
@@ -1,10 +1,8 @@
 require 'app'
+require 'infra/logger'
 
 Carpanta.configure do |config|
   config.root = ENV.fetch('ROOT_PATH')
   config.environment = ENV.fetch('RACK_ENV')
-
-  file = File.new(File.join(config.root, 'log', config.environment + '.log'), 'a+')
-  file.sync = true
-  config.logger = Logger.new(file)
+  config.logger = Infra::Logger.build
 end
---
+++ infra/logger.rb
@@ -0,0 +1,13 @@
+require 'logger'
+
+module Infra
+  class Logger < SimpleDelegator
+    class << self
+      def build
+        new(::Logger.new(STDOUT))
+      end
+    end
+
+    private_class_method :new
+  end
+end
