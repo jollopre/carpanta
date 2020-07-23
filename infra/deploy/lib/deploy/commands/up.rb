d85336ba948eec0b918520a1e83298e6b102e692
Jose Lloret <jose.lloret@flywire.com>
2020-07-23 09:17:20 +0200

    [#48] Deploy::Commands::Up WIP

--- infra/deploy/lib/deploy/commands/up.rb
+++ infra/deploy/lib/deploy/commands/up.rb
@@ -2,6 +2,7 @@ require 'dry-monads'
 require 'dry/monads/do'
 require 'deploy/schemas/up'
 require 'deploy/commands/create_cluster'
+require 'deploy/commands/register_task_definition'
 
 module Deploy
   module Commands
@@ -15,7 +16,14 @@ module Deploy
       end
 
       def call(params = {})
-        yield validate(params)
+        values = yield validate(params)
+        cluster_pairs = yield create_clusters(values)
+        task_definition_arns = yield register_task_definitions(values)
+
+        Success(
+          cluster: cluster_pairs,
+          task_definition: task_definition_arns
+        )
       end
 
       private
@@ -27,6 +35,49 @@ module Deploy
         return Failure(result.errors.to_h) if result.failure?
 
         Success(result.values)
+      end
+
+      def create_clusters(filtered_params)
+        resources = filtered_params.fetch(:resources)
+        clusters_params = resources.select do |_,v|
+          v.fetch(:type) == 'Aws::ECS::Cluster'
+        end
+
+        return Success([]) if clusters_params.empty?
+
+        logical_name = clusters_params.keys[0]
+        cluster_params = clusters_params.fetch(logical_name).fetch(:properties)
+        result = create_cluster_cmd.call(cluster_params)
+
+        return Failure(result.failure) if result.failure?
+
+        Success(
+          [{ logical_name: logical_name, arn: result.success }]
+        )
+      end
+
+      def create_cluster_cmd
+        @create_cluster_cmd ||= CreateCluster.new(client)
+      end
+
+      def register_task_definitions(filtered_params)
+        resources = filtered_params.fetch(:resources)
+        task_definitions_params = resources.select do |_,v|
+          v.fetch(:type) == 'Aws::ECS::TaskDefinition'
+        end.values
+
+        return Success([]) if task_definitions_params.empty?
+
+        task_definition_params = task_definitions_params.first.fetch(:properties)
+        result = register_task_definition_cmd.call(task_definition_params)
+
+        return Failure(result.failure) if result.failure?
+
+        Success([result.success])
+      end
+
+      def register_task_definition_cmd
+        @register_task_definition_cmd ||= RegisterTaskDefinition.new(client)
       end
     end
   end
--- infra/deploy/spec/deploy/commands/up_spec.rb
+++ infra/deploy/spec/deploy/commands/up_spec.rb
@@ -12,16 +12,21 @@ RSpec.describe Deploy::Commands::Up do
   end
 
   describe '#call' do
+    before do
+      client.stub_responses(:create_cluster, { cluster: { cluster_arn: 'a_cluster_arn' }})
+      client.stub_responses(:register_task_definition, { task_definition: { task_definition_arn: 'a_task_definition_arn' }})
+    end
+
     let(:default_params) do
       {
         resources: {
-          :"my_cluster" => {
+          my_cluster: {
             type: 'Aws::ECS::Cluster',
             properties: {
               cluster_name: 'a_cluster_name'
             }
           },
-          :"my_task" => {
+          my_task: {
             type: 'Aws::ECS::TaskDefinition',
             properties: {
               family: 'a_family',
@@ -38,18 +43,30 @@ RSpec.describe Deploy::Commands::Up do
     it 'creates a cluster' do
       result = subject.call(default_params)
 
-      skip('todo')
+      expect(result.success).to include(
+        cluster: include(
+          logical_name: :my_cluster,
+          arn: 'a_cluster_arn'
+        )
+      )
     end
 
     it 'creates a task definition' do
-      skip('todo')
+      result = subject.call(default_params)
+
+      expect(result.success).to include(
+        task_definition: include(
+          logical_name: :my_task,
+          arn: 'a_task_definition_arn'
+        )
+      )
     end
 
     context 'when params are invalid' do
       let(:params) do
         {
           resources: {
-            :"my_cluster" => {
+            my_cluster: {
               type: 'Aws::ECS::Cluster',
               properties: 'foo'
             }
@@ -68,10 +85,38 @@ RSpec.describe Deploy::Commands::Up do
 
         expect(result.failure).to include(
           resources: include(
-            :my_cluster => include(
+            my_cluster: include(
               properties: include('must be a hash')
             )
           )
+        )
+      end
+    end
+
+    context 'when there are no clusters defined under resources' do
+      before do
+        default_params.fetch(:resources).delete(:my_cluster)
+      end
+
+      it 'result returns an empty cluster' do
+        result = subject.call(default_params)
+
+        expect(result.success).to include(
+          cluster: []
+        )
+      end
+    end
+
+    context 'when there are no task_definitions defined under resources' do
+      before do
+        default_params.fetch(:resources).delete(:my_task)
+      end
+
+      it 'result returns an empty task_definition' do
+        result = subject.call(default_params)
+
+        expect(result.success).to include(
+          task_definition: []
         )
       end
     end
