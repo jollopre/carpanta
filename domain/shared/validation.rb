c0fefc0eb7a45e695457eb65ce9c8d67e9f79558
Jose Lloret <jose.lloret@flywire.com>
2020-10-04 11:56:10 +0200

    [#26] Domain::Shared::Validation using other shared modules

--- domain/shared/validation.rb
+++ domain/shared/validation.rb
@@ -1,25 +1,19 @@
 require 'dry-validation'
 require_relative 'resultable'
+require_relative 'callable'
 
 module Carpanta
   module Domain
     module Shared
       class Validation < Dry::Validation::Contract
-        class << self
-          include Resultable
+        extend Shared::Callable
+        include Shared::Resultable
 
-          def call(params = {})
-            result = contract.call(params)
-            return Failure(result.errors.to_h) unless result.errors.empty?
+        def call(params = {})
+          result = super
+          return Failure(result.errors.to_h) unless result.errors.empty?
 
-            Success(result.values.data)
-          end
-
-          private
-
-          def contract
-            @contract ||= new
-          end
+          Success(result.values.data)
         end
       end
     end
