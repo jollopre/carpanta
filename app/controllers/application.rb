5a0af6b2f6ec091797e3f183e14294231cd75f61
Jose Lloret <jose.lloret@flywire.com>
2020-12-14 07:43:53 +0100

    [#62] Prototype weekly calendar using grid display CSS

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,9 +1,11 @@
 require_relative 'base'
 require_relative 'customers'
+require_relative 'appointments'
 
 module Carpanta
   module Controllers
     class Application < Base
+      use Appointments
       use Customers
 
       get '/' do
